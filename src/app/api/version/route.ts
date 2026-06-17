fc7446b187b02342dd5d645d1501069ae82880d2
locpham <ploc280301@gmail.com>
2026-06-17 17:31:33 +0700

    update build failed, và vẫn ở v0.1.0

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -1,17 +1,21 @@
-import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   return NextResponse.json(
+//     {
+//       version: process.env.NEXT_PUBLIC_APP_VERSION || "v0.0.0",
+//       commit: process.env.NEXT_PUBLIC_COMMIT_SHA || "unknown",
+//       branch: process.env.NEXT_PUBLIC_BRANCH_NAME || "unknown",
+//       buildDate: process.env.NEXT_PUBLIC_APP_BUILD_DATE || "unknown",
+//     },
+//     {
+//       headers: {
+//         "Cache-Control": "no-store, no-cache, must-revalidate",
+//       },
+//     }
+//   );
+// }
 
 export async function GET() {
-  return NextResponse.json(
-    {
-      version: process.env.NEXT_PUBLIC_APP_VERSION || "v0.0.0",
-      commit: process.env.NEXT_PUBLIC_COMMIT_SHA || "unknown",
-      branch: process.env.NEXT_PUBLIC_BRANCH_NAME || "unknown",
-      buildDate: process.env.NEXT_PUBLIC_APP_BUILD_DATE || "unknown",
-    },
-    {
-      headers: {
-        "Cache-Control": "no-store, no-cache, must-revalidate",
-      },
-    }
-  );
+  // Cố tình ném lỗi để kiểm tra cơ chế rollback
+  throw new Error("Lỗi giả lập để test Rollback!");
 }
